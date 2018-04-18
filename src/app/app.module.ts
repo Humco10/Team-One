3f92f7b47bce55dd92e18eb24d2b3fa84fcb9e27
Alex Byrd <soccer43802@gmail.com>
2018-04-18 13:41:57 -0500

    First step of routing in place. Basic routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
+import { PageNotFoundComponent } from './not-found.component';
 
+
+const appRoutes: Routes = [
+  //{ path: 'applet', component: PagesComponent },
+  //{ path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    data: {
+      title: 'Login Through Twitter!',
+      pageNum: 1
+    }
+  },
+  {
+    path: 'applet',
+    component: PagesComponent,
+    data: {
+      title: 'Excelsior The Game!',
+      pageNum: 2
+    }
+  },
+  {
+    path: 'tweets',
+    component: PagesComponent,
+    data: {
+      title: 'Find a Tweet!',
+      pageNum: 3
+    }
+  },
+  {
+    path: 'random',
+    component: PagesComponent,
+    data: {
+      title: 'Random Shtuff!',
+      pageNum: 4
+    }
+  }
+  //{ path: '**', component: PageNotFoundComponent } //If path is not found
+  /*{ path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  }*/,
+  //{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,6 +55,10 @@ import { PagesComponent } from './pages/pages.component';
     PagesComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/not-found.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+	template: '<h2>Page not found: Try a different URL</h2>'
+})
+export class PageNotFoundComponent { }
