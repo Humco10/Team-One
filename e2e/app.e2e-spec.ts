3fcc3928ede97f7d90a9f6a19cfb0a28b5c9d4cf
Humberto Colin <colin010@live.missouristate.edu>
2018-05-04 16:13:24 -0500

    One e2e test previous commit was unit tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('my-app App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display Title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('HumByrd and Knapp Games');
   });
 });
