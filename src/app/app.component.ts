795c88c1014aafafc98ff4f78455fe4a9b50eff6
Humberto Colin <colin010@live.missouristate.edu>
2018-05-10 07:09:03 -0500

    changed location of html for header, footer and app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
-  template: `
-  <!-- header -->
-  <app-header></app-header>
-
-  <!-- routes will be rendered here -->
-  <router-outlet></router-outlet>
-
-  <!-- footer -->
-  <app-footer></app-footer>
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,23 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: `
-    <footer class="footer has-background-black fix-footer">
-    <div class="container">
-    <div class="content has-text-centered">
-      <p class="textColor">
-        @MasterCoder2112
-      </p>
-      <p class="textColor">
-        @Humco10
-      </p>
-      <p class="textColor">
-        @mk2961
-      </p>
-    </div>
-    </div>
-    </footer>
-  `,
+  templateUrl:'./footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
