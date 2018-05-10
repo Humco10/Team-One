ebc8f38af407ab499318808c9a6d7baf4d3abd7d
Humberto Colin <colin010@live.missouristate.edu>
2018-05-10 04:02:00 -0500

    Simple footer and css for it

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,17 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
-    <footer class="footer">
+    <footer class="footer has-background-black fix-footer">
     <div class="container">
     <div class="content has-text-centered">
-      <p>
-        
+      <p class="textColor">
+        @MasterCoder2112
+      </p>
+      <p class="textColor">
+        @Humco10
+      </p>
+      <p class="textColor">
+        @mk2961
       </p>
     </div>
     </div>
     </footer>
   `,
-  styles: []
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
